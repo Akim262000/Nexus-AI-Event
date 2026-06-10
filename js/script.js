109c60556268bea1233d12a61540368ede4b38ce
Akim Minko <akim.minko@yandex.ru>
2026-06-10 11:24:22 +0300

    feat: add section feedback

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ document
   .addEventListener("click", function () {
     this.classList.toggle("open");
   });
+
+$(".feedback-slider").slick({
+
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  variableWidth: false,
+});
